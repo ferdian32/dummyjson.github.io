8f213772e435b4dee21fad78804593bfb6c610e1
FierliFerdiansyah1 <fierliferdiansyah0@gmail.com>
2025-02-11 17:25:25 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/products");
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []); 
+
+  console.log(data?.products); 
+
+  return (
+    <section>
+      <h1>Data product</h1>
+      <ul>
+        {data?.products?.length > 0 ? (
+          data.products.map((item: any) => (
+            <li key={item.id}>{item.title}</li> 
+          ))
+        ) : (
+          <h1>Data Tidak ditemukan</h1>
+        )}
+      </ul>
+    </section>
+  );
+}
+
+export default App;
